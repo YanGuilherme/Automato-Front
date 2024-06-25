0753c09f4e6a4f6ffd7d53129c3999dff8f1ef0d
YAN <yanvzp@gmail.com>
2024-06-25 16:02:04 -0300

    edicao e exclusao funcionando

--- scripts.js
+++ scripts.js
@@ -58,18 +58,29 @@ function listar() {
 function update(event){
     event.preventDefault();
 
-    let id = document.getElementById('entrada_edit1').value;
-    let modelo = document.getElementById('entrada_edit2').value;
-    let preco = document.getElementById('entrada_edit3').value;
+    const id = document.getElementById('entrada_edit1').value;
+    const modelo = document.getElementById('entrada_edit2').value;
+    const preco = document.getElementById('entrada_edit3').value;
 
     if(!id){
-        alert("Por favor, fornece o ID do carro que deseja alterar.");
+        alert("Por favor, forneça o ID do carro que deseja alterar.");
+        return;
+    }
+
+    const dadosCarro = {};
+    if(modelo){
+        dadosCarro.modelo = modelo;
+    }
+    if(preco){
+        dadosCarro.preco = preco;
+    }
+
+    if(Object.keys(dadosCarro).length === 0){
+        alert('Por favor, forneça preencha pelo menos um capo para atualizar.');
         return;
     }
 
     const xhr = new XMLHttpRequest();
-
-
     xhr.open('PUT',`http://localhost:8080/update/${id}`,true);
     xhr.setRequestHeader('Content-Type','application/json');
 
@@ -79,20 +90,50 @@ function update(event){
                 console.log("Carro atualizado.");
                 listar(); // ja chama o metodo listar para ver como ficou a atualizacao
             }else{
-                console.error('Erro ao atualizar o carro', xhr.responseText);
+                console.error('Erro ao atualizar o carro.', xhr.responseText);
                 
             }
         }
     };
-    const dadosCarro = {
-        modelo: modelo,
-        preco: preco
-    };
 
-    xhr.send();
+
+    xhr.send(JSON.stringify(dadosCarro));
 
     document.getElementById('entrada_edit1').value = '';
     document.getElementById('entrada_edit2').value = '';
     document.getElementById('entrada_edit3').value = '';
+
+}
+
+function deletar(event){
+    event.preventDefault();
+    const id = document.getElementById('id_delete').value;
+    if(!id){
+        alert("Por favor, forneça o ID do carro que deseja deletar.");
+        return;
+    }
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('DELETE', `http://localhost:8080/delete/${id}`, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4){
+            if(xhr.status == 204){ // 204 no content
+                console.log("Carro deletado com sucesso.");
+                listar();
+            }else{
+                console.error("Erro ao deletar o carro.", xhr.responseText);
+            }
+        }
+    };
+
+    xhr.send();
+
+    document.getElementById('id_delete').value = '';
+
+
+
+
 
 }
